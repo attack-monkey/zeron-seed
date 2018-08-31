e02ff9ad8b757bf07abc8b2b88bd6e451aca1858
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-09-01 07:00:07 +0930

    check if background has already loaded

--- components/view-port/leaf/leaf.component.ts
+++ components/view-port/leaf/leaf.component.ts
@@ -2,7 +2,13 @@ import { component } from "zeron/functions/component.function";
 import { $ } from "zeron/functions/$.function";
 import { leafTemplate } from "./leaf.template";
 import { pushStateRoutes } from 'zeron/functions/push-state-routes.function';
+import { getState, state } from "../../../node_modules/zeron/functions/store.function";
+import { iu } from "../../../node_modules/zeron/functions/iu.functions";
 
 export function leafComponent() {
-    component($('body'), leafTemplate());
+    // Only load background if it hasn't already been loaded - otherwise ie will flicker on route changes
+    if (getState('leafComponentLoaded') !== true) {
+        component($('body'), leafTemplate());
+        state().set(iu(getState(), 'leafComponentLoaded', true));
+    }
 }
