c41c4c3dc6b6e428205273e944b720b4e4d3b1f3
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-09-28 05:32:17 +0930

    Changes to make use of new features

--- components/view-port/leaf/leaf.component.ts
+++ components/view-port/leaf/leaf.component.ts
@@ -1,14 +1,10 @@
-import { component } from "zeron/functions/component.function";
-import { $ } from "zeron/functions/$.function";
+import { component, $, getState, state, iu, updateState } from "zeron";
 import { leafTemplate } from "./leaf.template";
-import { pushStateRoutes } from 'zeron/functions/push-state-routes.function';
-import { getState, state } from "../../../node_modules/zeron/functions/store.function";
-import { iu } from "../../../node_modules/zeron/functions/iu.functions";
 
 export function leafComponent() {
     // Only load background if it hasn't already been loaded - otherwise ie will flicker on route changes
     if (getState('leafComponentLoaded') !== true) {
         component($('body'), leafTemplate());
-        state().set(iu(getState(), 'leafComponentLoaded', true));
+        updateState('leafComponentLoaded', true);
     }
 }
